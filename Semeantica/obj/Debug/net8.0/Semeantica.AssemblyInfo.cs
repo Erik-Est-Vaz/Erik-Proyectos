781e665779c0f650083524f22348a613d0d83cbe
MauricioHer095 <l22140774@queretaro.tecnm.mx>
2024-09-11 09:41:07 -0600

    prueba001

--- Semeantica/obj/Debug/net8.0/Semeantica.AssemblyInfo.cs
+++ Semeantica/obj/Debug/net8.0/Semeantica.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Semeantica")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+082da7af9a08c3400d258e13b497e79c73fa21e6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c817155d0e7a185161211b1874fb912fd2ea68a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Semeantica")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Semeantica")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
